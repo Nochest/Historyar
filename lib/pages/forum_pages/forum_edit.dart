fe5fd0cf4cfb1b1f4abbfa130a53ef5ab17803de
Nochest <camogliano.97@gmail.com>
2022-04-15 22:47:56 -0500

    RA changes

--- lib/pages/forum_pages/forum_edit.dart
+++ lib/pages/forum_pages/forum_edit.dart
@@ -17,24 +17,19 @@ import 'package:historyar_app/widgets/input_text.dart';
 import 'package:intl/intl.dart';
 
 class ForumEdit extends StatefulWidget {
-
   final int id;
   final int type;
   final int userId;
 
-  const ForumEdit({
-    required this.id,
-    required this.type,
-    required this.userId,
-    Key? key
-  }) : super(key: key);
+  const ForumEdit(
+      {required this.id, required this.type, required this.userId, Key? key})
+      : super(key: key);
 
   @override
   _ForumEditState createState() => _ForumEditState();
 }
 
 class _ForumEditState extends State<ForumEdit> {
-
   ColorPalette _colorPalette = ColorPalette();
   InputText _inputText = InputText();
   Alert _alert = Alert();
@@ -80,77 +75,78 @@ class _ForumEditState extends State<ForumEdit> {
     });
 
     return Scaffold(
-      backgroundColor: _colorPalette.cream,
-      appBar: AppBar(
-        backgroundColor: _colorPalette.darkBlue,
-        title:
-        Text('Editar Publicación', style: TextStyle(fontWeight: FontWeight.w700)),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.of(context).pushAndRemoveUntil(
-                MaterialPageRoute(builder: (BuildContext context) => MyForums(id: widget.id, type: widget.type)),
-                    (Route<dynamic> route) => false);
-          },
+        backgroundColor: _colorPalette.cream,
+        appBar: AppBar(
+          backgroundColor: _colorPalette.darkBlue,
+          title: Text('Editar Publicación',
+              style: TextStyle(fontWeight: FontWeight.w700)),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(
+                      builder: (BuildContext context) =>
+                          MyForums(id: widget.userId, type: widget.type)),
+                  (Route<dynamic> route) => false);
+            },
+          ),
         ),
-      ),
-      body: isLoading
-          ? Center(
-        child: CircularProgressIndicator(),
-      )
-          : SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 24.0),
-          child: Container(
-            alignment: Alignment.center,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  alignment: Alignment.topLeft,
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 24.0),
-                    child: Text(
-                      'Ingrese el tema y descripción',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w700,
-                          color: _colorPalette.yellow,
-                          fontSize: 32.0),
-                      textAlign: TextAlign.start,
+        body: isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : SingleChildScrollView(
+                child: Padding(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 5.0, horizontal: 24.0),
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          alignment: Alignment.topLeft,
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 24.0),
+                            child: Text(
+                              'Ingrese el tema y descripción',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  color: _colorPalette.yellow,
+                                  fontSize: 32.0),
+                              textAlign: TextAlign.start,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 24.0),
+                          child: _inputText.defaultIText(
+                              focus_topic,
+                              _topicController,
+                              TextInputType.text,
+                              'Tema',
+                              '',
+                              false,
+                              'Tema',
+                              topic),
+                        ),
+                        Padding(
+                            padding: EdgeInsets.only(top: 40.0),
+                            child: TextField(
+                                controller: _descriptionController,
+                                keyboardType: TextInputType.multiline,
+                                maxLines: null,
+                                focusNode: focus_description,
+                                decoration:
+                                    InputDecoration(labelText: 'Descripción'))),
+                        Padding(
+                            padding: EdgeInsets.only(top: 22.0, bottom: 40.0),
+                            child: _saveButton(context)),
+                      ],
                     ),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(top: 24.0),
-                  child: _inputText.defaultIText(
-                      focus_topic,
-                      _topicController,
-                      TextInputType.text,
-                      'Tema',
-                      '',
-                      false,
-                      'Tema',
-                      topic),
-                ),
-                Padding(
-                    padding: EdgeInsets.only(top: 40.0),
-                    child: TextField(
-                        controller: _descriptionController,
-                        keyboardType: TextInputType.multiline,
-                        maxLines: null,
-                        focusNode: focus_description,
-                        decoration: InputDecoration(labelText: 'Descripción')
-                    )
-                ),
-                Padding(
-                    padding: EdgeInsets.only(top: 22.0, bottom: 40.0),
-                    child: _saveButton(context)),
-              ],
-            ),
-          ),
-        ),
-      )
-    );
+              ));
   }
 
   Widget _saveButton(BuildContext context) {
@@ -167,9 +163,13 @@ class _ForumEditState extends State<ForumEdit> {
           onPressed: () {
             if (_topicController.text.isNotEmpty &&
                 _descriptionController.text.isNotEmpty) {
-              _foroProvider.editarPublicacion(widget.id ,_topicController.text,
+              _foroProvider.editarPublicacion(
+                  widget.id,
+                  _topicController.text,
                   _descriptionController.text,
-                  widget.userId, widget.type, context);
+                  widget.userId,
+                  widget.type,
+                  context);
               //Navigator.of(context).pushReplacement(MaterialPageRoute(
               //   builder: (BuildContext context) => SignIn()));
             } else {
